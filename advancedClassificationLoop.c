fa21788fe358b2677d22ce6e520683e279909da8
Michal <michalruth144@gmail.com>
2022-11-23 19:08:48 +0200

    fix functions isArmstrong and isPalindrom

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -27,15 +27,31 @@ else{
 
 
 int isPalindrome(int num){
-int count=countDigits(num);
-int bool=1;
-while((count/2) && bool){ // count/2>0
-    if(num%10 != num/(calcPower(10,count-1)))
-        bool=0;
-    num= (num % calcPower(10,count-1) ) /10;  // remove the last and first digit of num
-    count=count-2;
+    int original=num;
+    int reverse=0;
+    //int order=1;
+
+    while(num){
+        reverse= reverse*10+num%10;
+        num=num/10;
+      //  order=order+1;
+    }
+    if(reverse==original){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+
 }
-return bool;
+// int count=countDigits(num);
+// int bool=1;
+// while((count/2) && bool){ // count/2>0
+//     if(num%10 != num/(calcPower(10,count-1)))
+//         bool=0;
+//     num= (num % calcPower(10,count-1) ) /10;  // remove the last and first digit of num
+//     count=count-2;
+// }
+// return bool;
 
 
-}
