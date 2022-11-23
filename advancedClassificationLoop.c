fd732f6ab3af5105e712f3b4961ece57b4861002
Michal <michalruth144@gmail.com>
2022-11-23 20:59:36 +0200

    add edge cases to functions

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -1,7 +1,7 @@
 
 
 int countDigits(int ); 
-int calcPower(int,int ); 
+double pow(double,double); 
 
 
 int isArmstrong(int num ){
@@ -9,7 +9,7 @@ int original = num;
 int power=countDigits(num);
 int sumPower=0;
 while (num>0){
-    sumPower= sumPower+ calcPower(num%10, power);
+    sumPower= sumPower+ pow(num%10, power);
     num=num/10;
 }
 if(sumPower== original){
@@ -31,7 +31,7 @@ int isPalindrome(int num){
     int reverse=0;
     //int order=1;
 
-    while(num){
+    while(num>0){
         reverse= reverse*10+num%10;
         num=num/10;
       //  order=order+1;
@@ -44,14 +44,4 @@ int isPalindrome(int num){
     }
 
 }
-// int count=countDigits(num);
-// int bool=1;
-// while((count/2) && bool){ // count/2>0
-//     if(num%10 != num/(calcPower(10,count-1)))
-//         bool=0;
-//     num= (num % calcPower(10,count-1) ) /10;  // remove the last and first digit of num
-//     count=count-2;
-// }
-// return bool;
-
 
--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,49 +1,33 @@
 //#include <math.h>
 int countDigits(int ); 
-int calcPower(int,int ); 
+//int calcPower(int,int ); 
 int reverse(int );
 double log10(double);
 double pow(double , double);
 // int armstrong(int , int );
 
 
-
-// // a helping recursion function for isArmstrong
-// int armstrong(int num, int numDigits){
-//     if(num==0){ // if num==0
-//         return 0;
-//     }
-//     else
-//         return calcPower((num%10),numDigits)+ armstrong(num/10,numDigits);
-// }
-
-// int isArmstrong(int num ){
-//     int ans = armstrong(num, countDigits(num));
-//     if(ans){
-//         return 1;
-//     }
-//     else{
-//         return 0;
-//     }
-// }
+int isArmHelp(int num, int count){
+    if(num<10){ // if it's a digit, return it using pow() function
+        return pow(num,count);
+    }
+    else{
+        return(pow(num%10,count)+isArmHelp(num/10,count));
+    }
+}
 
 int isArmstrong(int num ){
-int original = num;
-int power=countDigits(num);
-int sumPower=0;
-while (num>0){
-    sumPower= sumPower+ calcPower(num%10, power); // recursion
-    num=num/10;
-}
-if(sumPower== original){
-    return 1;
-}
-else{
-    return 0;
-}
+     int count = countDigits(num);
+     if(num == isArmHelp(num,count)){
+        return 1;
+     }
+     else{
+        return 0;
+     }
 }
 
 
+// helping function for isPalindrom
 int reverse(int num){ // order= num_of_Digits-1
     int order=(int)log10(num);
     if(num==0){
@@ -53,7 +37,7 @@ int reverse(int num){ // order= num_of_Digits-1
     return ((num%10)*(int)(pow(10,order))+reverse(num/10));
  
 
-} // helping function
+} 
 
  ///  this function checks if a number is palindrom recursively
  int isPalindrome(int num){
@@ -69,15 +53,7 @@ int reverse(int num){ // order= num_of_Digits-1
  }
 
 
-    // if(countDigits(num)<2){
-    //     return 1;
-    // }
-
-    // if(num%10 == (num / (calcPower(10,countDigits(num)-1))))
-    //     return isPalindrome((num % calcPower(10,countDigits(num)-1 ) )/10); // taking down the first and last digits
-    // else
-    //     return 0;    
-
+  
 
 
 
