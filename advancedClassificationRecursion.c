9c4615803d29f6ca76c2f34f874fbfa7476e7cf3
Michal <michalruth144@gmail.com>
2022-11-23 19:10:26 +0200

    fix function isPalindrom

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,6 +1,9 @@
-
+//#include <math.h>
 int countDigits(int ); 
 int calcPower(int,int ); 
+int reverse(int );
+double log10(double);
+double pow(double , double);
 // int armstrong(int , int );
 
 
@@ -29,7 +32,7 @@ int original = num;
 int power=countDigits(num);
 int sumPower=0;
 while (num>0){
-    sumPower= sumPower+ calcPower(num%10, power);
+    sumPower= sumPower+ calcPower(num%10, power); // recursion
     num=num/10;
 }
 if(sumPower== original){
@@ -41,20 +44,39 @@ else{
 }
 
 
+int reverse(int num){ // order= num_of_Digits-1
+    int order=(int)log10(num);
+    if(num==0){
+        return 0;
+    }
+    
+    return ((num%10)*(int)(pow(10,order))+reverse(num/10));
+ 
+
+} // helping function
 
  ///  this function checks if a number is palindrom recursively
  int isPalindrome(int num){
-    if(countDigits(num)<2){
+    int ans=reverse(num);
+    if(ans==num){
         return 1;
     }
-
-    if(num%10 == (num / (calcPower(10,countDigits(num)-1))))
-        return isPalindrome((num % calcPower(10,countDigits(num)-1 ) )/10); // taking down the first and last digits
-    else
-        return 0;    
+    else{
+        return 0;
+    }
 
 
-}
+ }
+
+
+    // if(countDigits(num)<2){
+    //     return 1;
+    // }
+
+    // if(num%10 == (num / (calcPower(10,countDigits(num)-1))))
+    //     return isPalindrome((num % calcPower(10,countDigits(num)-1 ) )/10); // taking down the first and last digits
+    // else
+    //     return 0;    
 
 
 
