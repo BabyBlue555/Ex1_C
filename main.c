129e4a49c8ceea871739fa29bc30e07f0f42b875
Michal <michalruth144@gmail.com>
2022-11-24 09:29:54 +0200

    change sintax

--- main.c
+++ main.c
@@ -4,67 +4,6 @@
 
 
 int main(){
-
-// int num1=0;
-// int num2=0;
-// int max=0;
-// int min=0;
-
-
-
-// scanf("%d",&num1);
-// scanf("%d",&num2);
-
-
-
-// min = num1<num2 ? num1 : num2;
-// max = num1>num2 ? num1: num2;
-
-// int i=0;
-// //printf("\n");
-
-// printf("The Armstrong numbers are: ");
-//   for(i=min;i<=max;i++){
-//       if(isArmstrong(i)){
-//             printf("%d " , i );
-       
-    
-//       }
-//   }
-// //printf("\n");
-
-// printf("\nThe Palindromes are: ");   
-// for(i=min;i<=max;i++){
-//     if(isPalindrome(i)){ 
-//           printf( "%d ", i);
-        
-    
-//     }
-// }
-// //printf("\n");
-
-
-// printf("\nThe Prime numbers are: ");
-// for(i= min;i<=max;i++){
-//         int ans= isPrime(i);
-//         if(ans){
-//           printf("%d " , i );
-      
-        
-//         }
-// }
-// // printf("\n");
-
-// printf("\nThe Strong numbers are: ");
-// for(i=min;i<=max;i++){
-//     if(isStrong(i)){ // power
-//             printf("%d " , i );
-       
-    
-//     }
-// }
-// printf("\n");
-
     int num1, num2;
     scanf("%d %d", &num1, &num2);
     int max = num2, min = num1;
